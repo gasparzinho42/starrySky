339c4edce4f5ce264d36f89d2579166201cdbcba
gasparzinho42 <andrews.3257@outlook.com>
2020-10-11 18:18:45 -0300

    I think its quite beautiful :)

---
+++ README.md
@@ -0,0 +1,3 @@
+It's a simple front-end animations Test.
+    if you want to run it, simply clone this repo to you desktop
+    open it on your editor then run "yarn start" on the command prompt :)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>Bem vindo ao mundo das animações</h1>
+      <small>céu estrelado</small>
+    </main>
+  );
+}
+
+export default App;
